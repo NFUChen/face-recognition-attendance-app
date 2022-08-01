a89c5e0545433e86748f883dd53312718630f8d4
William Chen <10979111@gm.nfu.edu.tw>
2022-08-02 01:33:44 +0800

    refactor the code in face_recognizer (i.e., removing duplicate code)

--- face_recognizer.py
+++ face_recognizer.py
@@ -50,21 +50,13 @@ class FaceRecognizer:
             #如有現有的array文件可以用
             if os.path.exists(array_file_path):
                 img_encoding = np.load(array_file_path)
-                self.known_face_encodings.append(img_encoding)
-                self.known_face_names.append(filename)
-                print(f"Adding {filename} to system...")
-                self.human_resources_system.add_employee(filename)
-                continue
+            else:
+                img = cv2.imread(img_path)
+                rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                # Get encoding
+                img_encoding = face_recognition.face_encodings(rgb_img).pop()
+                np.save(f'./images_encoding/{filename}', img_encoding)
 
-            img = cv2.imread(img_path)
-            rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-            
-            # Get encoding
-            img_encoding = face_recognition.face_encodings(rgb_img).pop()
-            np.save(f'./images_encoding/{filename}', img_encoding)
-
-            
             print(f"Adding {filename} to system...")
             # Store file name and file encoding
             self.known_face_encodings.append(img_encoding)
